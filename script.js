a60fabc43e82b37e7f4aa3f8ed51ad48442b23cb
waskitazaenal17-collab! <waskitazaenal17@gmail.com>
2026-06-22 19:31:59 +0700

    Upload foto asli dan update code

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const themeToggleBtn = document.getElementById('theme-toggle');
 const body = document.body;
 const icon = themeToggleBtn.querySelector('i');
 
-// Mengecek apakah sebelumnya user sudah memilih dark mode (disimpan di browser)
+// Mengecek preferensi tema terakhir dari browser user
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme === 'dark') {
     body.classList.add('dark-mode');
@@ -11,12 +11,11 @@ if (currentTheme === 'dark') {
     icon.classList.add('fa-sun');
 }
 
-// Menambahkan event ketika tombol diklik
+// Menjalankan fungsi ganti tema saat tombol diklik
 themeToggleBtn.addEventListener('click', () => {
-    // Toggle class 'dark-mode' di body
     body.classList.toggle('dark-mode');
     
-    // Ganti ikon bulan/matahari dan simpan preferensi ke local storage
+    // Ganti ikon bulan/matahari dan simpan ke local storage
     if (body.classList.contains('dark-mode')) {
         icon.classList.remove('fa-moon');
         icon.classList.add('fa-sun');
